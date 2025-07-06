f0f7fdad88addc2b5570c169c87251b3a05428dc
MikailaAkeredolu <makerofappsdotcom@gmail.com>
2025-07-06 11:37:01 -0400

    Initial Commit

---
+++ FireFetch/FireFetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireFetchApp.swift
+//  FireFetch
+//
+//  Created by mikaila Akeredolu on 7/6/25.
+//
+
+import SwiftUI
+
+@main
+struct FireFetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
